b871f44f7113bb6d8dbac920f6be560412e0bc0a
JakubKivi <panmcleon@gmail.com>
2019-01-12 22:42:33 +0100

    Dodanie wszyskich plików potrzebnych do działania programu

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
-/*==============[TO JEST SUPER GRA KTÓRĄ ROMIMY W FERIE]====================*/
-#include "iostream"
-using namespace std;
+#include <SFML/Window.hpp>
 
-int main(){
-	cout << "Hello World!" << endl;
-	return 0;
+int main()
+{
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
+    return 0;
 }
