cdc10170368f9e3ec47c538909d26365cab75f81
JakubKivi <panmcleon@gmail.com>
2019-01-17 16:09:15 +0100

    abyss

--- Figures.h
+++ Figures.h
@@ -9,19 +9,26 @@
 
     pole przepaœci  - notexist
     polec puste     - empty
+    przepaœæ        -abyss
 */
-
+int abs(int x){
+    return x > 0 ? x: -x;
+}
 
 
 bool CanMove(Pole board[17][34], int x, int y, int targetX, int targetY){
     if(board[x][y].name=="charge"){
-        if(board[targetX][targetY].name=="empty") return 1;
-            if( y==tagetY ){
-                if targetX-x==2
+        if(board[targetX][targetY].name=="empty"){
+            if( y==targetY && abs(targetX-x)==2 ) return 1;
+            if( x==targetX){
+                for(int i=y; i<targetY; )
             }
-            else
-        else return 0;
-    }
+        }
+
+        }else return 0;
+
+
+
     if(board[x][y].name=="tower"){
          if(board[targetX][targetY].name=="empty") return 1;
 
