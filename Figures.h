36c134c0859a563ca4d1475fde35a82b5ca2c4ce
JakubKivi <panmcleon@gmail.com>
2019-01-17 18:27:30 +0100

    canMove dla wiezy

--- Figures.h
+++ Figures.h
@@ -32,9 +32,16 @@ bool CanMove(Pole board[17][34], int x, int y, int targetX, int targetY){
 
 
     if(board[x][y].name=="tower"){
-         if(board[targetX][targetY].name=="empty") return 1;
+         if(board[targetX][targetY].name=="empty"){
+            if(targetX==x){
+                if(abs(targetY-y)==2) return 1;
+                if(targetY-y==4&&board[x][y+2].name=="empty") return 1;
+                if(targetY-y==-44&&board[x][y-2].name=="empty") return 1;
+            }
+            if(abs(targetX-x)==1 && abs(targetY-y)==1) return 1;
+            if(abs(targetX-x)==2 && abs(targetY-y)==2) return 1;
 
-         else return 0;
+         }else return 0;
     }
 
 
