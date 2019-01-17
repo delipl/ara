1330791c56c656765ce0292b64b3e9d46ee03be6
delipl <delicat.kuba@gmail.com>
2019-01-17 16:26:29 +0100

    canMove ducha i kawalerii

--- Figures.h
+++ Figures.h
@@ -28,11 +28,62 @@ bool CanMove(Pole board[17][34], int x, int y, int targetX, int targetY){
     }
     if(board[x][y].name=="ghost"){
         if(board[targetX][targetY].name=="empty"){
-            return abs(targetY - x) == 3 && abs(targetY - y) == 1? true: false ||
+            return  (abs(targetY - x) == 3 && abs(targetY - y) == 1) ||
+                    (abs(targetY - x) == 1 && abs(targetY - y) == 3) ||
+                    (abs(targetY - x) == 2 && abs(targetY - y) == 0) ?
+                    true: false;
         }
-
         else return 0;
     }
 
+
+    if(board[x][y].name=="cav"){
+        if(board[targetX][targetY].name == "empty"){
+
+            if(x > targetX && y < targetY){
+                for(int i = x; i < 17;i++){
+                    for (int j = y; j >= 0; --j){
+                        //przeskakiwanie przez wrogow zrobie w canAttack()
+                        if (board[i][j].name != "empty" && board[i][j].name != "notexist"){
+                            return 0;
+                        }
+                    }
+                }
+                return 1;
+            }
+
+            if(x > targetX && y > targetY){
+                for(int i = x; i < 17;i++){
+                    for (int j = y; j < 33; ++j){
+                        //przeskakiwanie przez wrogow zrobie w canAttack()
+                        if (board[i][j].name != "empty" && board[i][j].name != "notexist") return 0;
+                    }
+                }
+                return 1;
+            }
+
+            if(x < targetX && y > targetY){
+                for(int i = x; i >= 0; --i){
+                    for (int j = y; j < 33; ++j){
+                        //przeskakiwanie przez wrogow zrobie w canAttack()
+                        if (board[i][j].name != "empty" && board[i][j].name != "notexist") return 0;
+                    }
+                }
+                return 1;
+            }
+
+            if(x < targetX && y < targetY){
+                for(int i = x; i >= 0;i--){
+                    for (int j = y; j >= 0; --j){
+                        //przeskakiwanie przez wrogow zrobie w canAttack()
+                        if (board[i][j].name != "empty" && board[i][j].name != "notexist") return 0;
+                    }
+                }
+                return 1;
+            }
+
+        }
+        else return 0;
+    }
 
 }
