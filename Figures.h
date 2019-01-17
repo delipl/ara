7af0ef2835bedb30805cae44105074af9572fef3
JakubKivi <panmcleon@gmail.com>
2019-01-17 16:16:57 +0100

    dodano canMove do figury charge

--- Figures.h
+++ Figures.h
@@ -9,7 +9,7 @@
 
     pole przepaœci  - notexist
     polec puste     - empty
-    przepaœæ        -abyss
+    przepaœæ        - abyss
 */
 int abs(int x){
     return x > 0 ? x: -x;
@@ -21,11 +21,13 @@ bool CanMove(Pole board[17][34], int x, int y, int targetX, int targetY){
         if(board[targetX][targetY].name=="empty"){
             if( y==targetY && abs(targetX-x)==2 ) return 1;
             if( x==targetX){
-                for(int i=y; i<targetY; )
+                for(int i=y; i<targetY; i++){
+                  if(!(board[x][i].name=="empty"||board[x][i].name=="notexist")) return 0;
+                }
+                return 1;
             }
         }
-
-        }else return 0;
+    }else return 0;
 
 
 
