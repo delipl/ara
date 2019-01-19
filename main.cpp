c8bbff1ef126465dfb45915c030d7a8866f8a4f6
Adam Wieczarek <adamwie12@gmail.com>
2019-01-19 11:26:45 +0100

    Jakieś stare twory

--- main.cpp
+++ main.cpp
@@ -19,13 +19,7 @@ int main()
 //=================================T£O================================================//
     HWND hWnd = GetConsoleWindow();
 
-
     sf::Texture Background;
-    if (!Background.loadFromFile("img/dupa.png"))
-    {
-
-    }
-
     sf::Texture texture_pawn;
     sf::Texture texture_ghost;
     sf::Texture texture_cav;
@@ -36,6 +30,10 @@ int main()
     sf::Texture texture_notexist;
     sf::Texture texture_nothing;
 
+    if (!Background.loadFromFile("img/dupa.png"))
+    {
+
+    }
     //wczytuje teksturki pionkow
         if (!texture_pawn.loadFromFile("img/pawn.png"))
         {
@@ -91,8 +89,8 @@ int main()
     front_fields = new Pole [578];
 
     Pole klik;
-    klik.setPosition(sf::Vector2f(680, 350));
-    klik.setScale(sf::Vector2f(0.2, 0.2));
+    klik.setPosition(sf::Vector2f(680.0f, 350.0f));
+    klik.setScale(sf::Vector2f(0.2f, 0.2f));
     klik.setTexture(Background);
     klik.setColor(sf::Color::Red);
 
@@ -108,19 +106,19 @@ int main()
             if(i%2==j%2)
             {
                 background_fields[i][j].setTexture(Background);
-                background_fields[i][j].setScale(sf::Vector2f(0.2, 0.2));
-                background_fields[i][j].setOrigin(sf::Vector2f(0.1, 0.1));
-                background_fields[i][j].setPosition(sf::Vector2f(i*30,j*20));
-                front_fields[i * board_size_y + j].setScale(sf::Vector2f(0.2, 0.2));
-                front_fields[i * board_size_y + j].setOrigin(sf::Vector2f(0.1, 0.1));
-                front_fields[i * board_size_y + j].setPosition(sf::Vector2f(i*30,j*20));
+                background_fields[i][j].setScale(sf::Vector2f(0.2f, 0.2f));
+                background_fields[i][j].setOrigin(sf::Vector2f(0.1f, 0.1f));
+                background_fields[i][j].setPosition(sf::Vector2f(i*30.0f,j*20.0f));
+                front_fields[i * board_size_y + j].setScale(sf::Vector2f(0.2f, 0.2f));
+                front_fields[i * board_size_y + j].setOrigin(sf::Vector2f(0.1f, 0.1f));
+                front_fields[i * board_size_y + j].setPosition(sf::Vector2f(i*30.0f,j*20.0f));
             }
             else
             {
                 background_fields[i][j].setPosition(sf::Vector2f(10000, 10000));
-                background_fields[i][j].setOrigin(sf::Vector2f(0.1, 0.1));
+                background_fields[i][j].setOrigin(sf::Vector2f(0.1f, 0.1f));
                 front_fields[i * board_size_y + j].setPosition(sf::Vector2f(10000, 10000));
-                front_fields[i * board_size_y + j].setOrigin(sf::Vector2f(0.1, 0.1));
+                front_fields[i * board_size_y + j].setOrigin(sf::Vector2f(0.1f, 0.1f));
             }
         }
     }
@@ -216,10 +214,10 @@ int main()
 
     // Tryb play
 
-    int figure_x = 0;
-    int figure_y = 0;
-    int target_x = 0;
-    int target_y = 0;
+    int figure_x = 0.0f;
+    int figure_y = 0.0f;
+    int target_x = 0.0f;
+    int target_y = 0.0f;
 
     while (window.isOpen())
     {
@@ -307,7 +305,7 @@ int main()
                     }
                 }
 
-                if(pow(mouse_position.x - 20 - background_fields[i][j].getPosition().x, 2) + pow(mouse_position.y - 20 - background_fields[i][j].getPosition().y, 2) < 400)
+                if(pow(mouse_position.x - 20.0f - background_fields[i][j].getPosition().x, 2) + pow(mouse_position.y - 20.0f - background_fields[i][j].getPosition().y, 2) < 400.0f)
                 {
                     background_fields[i][j].setColor(sf::Color::Green);
                 }
