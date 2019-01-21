e4104e0f25fac71a13ced7f6e54038d8f095d2ff
delipl <delicat.kuba@gmail.com>
2019-01-21 17:14:00 +0100

    szarza moze zakonczyc gre

--- Figures.h
+++ Figures.h
@@ -286,6 +286,11 @@ void Move(Pole *wsk_to_board, int x, int y, int targetX, int targetY)
 
                 for(int i = 1; i < abs(targetY - y); i ++)
                 {
+                    if( wsk_to_board[x * 34 + (i * d_y) + y].name   = "king"    &&
+                        wsk_to_board[x * 34 + (i * d_y) + y].owner != wsk_to_board[x * 34 + y].owner)
+                        win = 1;
+
+
                     wsk_to_board[x * 34 + (i * d_y) + y].name = "empty";
                     wsk_to_board[x * 34 + (i * d_y) + y].owner = 0;
                     //board[x][i * d_y + y].setTexture(Background);
@@ -306,7 +311,7 @@ void Move(Pole *wsk_to_board, int x, int y, int targetX, int targetY)
 
             if(board[targetX - d_x][targetY - d_y].owner == opponent_owner)
             {
-                if(wsk_to_board[(targetX - d_x) * 34 + targetY - d_y].name) win = 1;
+                if(board[targetX - d_x][targetY - d_y].name == "king") win = 1;
                 wsk_to_board[(targetX - d_x) * 34 + targetY - d_y].name = "empty";
                 wsk_to_board[(targetX - d_x) * 34 + targetY - d_y].owner = 0;
                 //board[targetX - d_x][targetY - d_y].setTexture(Background);
@@ -314,9 +319,9 @@ void Move(Pole *wsk_to_board, int x, int y, int targetX, int targetY)
                 {
                     if(board[targetX - (3 * d_x)][targetY - (3 * d_y)].owner == opponent_owner)
                     {
-                        if(wsk_to_board[(targetX - (3 * d_x)) * 34 + targetY - (3 * d_y)].name) win = 1;
-                        wsk_to_board[(targetX - (3 * d_x)) * 34 + targetY - (3 * d_y)].name = "empty";
-                        wsk_to_board[(targetX - (3 * d_x)) * 34 + targetY - (3 * d_y)].owner = 0;
+                        if(board[targetX - d_x][targetY - d_y].name == "king")         win      = 1;
+                        wsk_to_board[(targetX - (3 * d_x)) * 34 + targetY - (3 * d_y)].name     = "empty";
+                        wsk_to_board[(targetX - (3 * d_x)) * 34 + targetY - (3 * d_y)].owner    = 0;
                         //board[targetX - (3 * d_x)][targetY - (3 * d_y)].setTexture(Background);
                     }
                 }
