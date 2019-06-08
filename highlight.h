62d3bc891e8214308f194384a1196362fe517861
priestoov <kamamias@autograf.pl>
2019-06-08 12:49:22 +0200

    wieza nie podswietla celu

--- highlight.h
+++ highlight.h
@@ -9,7 +9,7 @@
 
 
 bool highlight(int x, int y){
-    
+
 
     int opponentOwner;
     (front_fields[x* 34 + y].owner == 1)?opponentOwner = 2:opponentOwner = 1;
@@ -24,7 +24,7 @@ bool highlight(int x, int y){
                                     front_fields[x*34+l].owner == opponentOwner)
                                         background_fields[x][l].setColor(sf::Color::Red);
                                 else if(canMove(front_fields, x, y, x, l)   &&
-                                    front_fields[x*34+l].name == empty)  
+                                    front_fields[x*34+l].name == empty)
                                         background_fields[x][l].setColor(sf::Color::Green);
                         }
                     }
@@ -33,10 +33,10 @@ bool highlight(int x, int y){
                 tura==1?a=1:a=-1;
                 if(x>0){
                     if(canAttack(front_fields, x, y, x-1, y+a)      &&
-                        front_fields[(x-1)*34+y+a].owner == opponentOwner)     
+                        front_fields[(x-1)*34+y+a].owner == opponentOwner)
                             background_fields[x-1][y+a].setColor(sf::Color::Red);
                     else if(canMove(front_fields, x, y, x-1, y+a)   &&
-                        front_fields[(x-1)*34+y+a].name == empty)  
+                        front_fields[(x-1)*34+y+a].name == empty)
                             background_fields[x-1][y+a].setColor(sf::Color::Green);
                 }
                 if(x<16){
@@ -44,10 +44,10 @@ bool highlight(int x, int y){
                         front_fields[(x+1)*34+y+a].owner == opponentOwner)
                             background_fields[x+1][y+a].setColor(sf::Color::Red);
                     else if(canMove(front_fields, x, y, x+1, y+a)   &&
-                        front_fields[(x+1)*34+y+a].name == empty)  
+                        front_fields[(x+1)*34+y+a].name == empty)
                             background_fields[x+1][y+a].setColor(sf::Color::Green);
                 }
-    } 
+    }
 
     //duch
     else if(front_fields[x* 34 + y].name == "ghost"){
@@ -109,7 +109,7 @@ bool highlight(int x, int y){
                 ny--;
                 if(canAttack(front_fields, x, y, nx, ny)    &&
                     front_fields[nx*34+ny].owner == opponentOwner)
-                        background_fields[nx][ny].setColor(sf::Color::Red);            
+                        background_fields[nx][ny].setColor(sf::Color::Red);
             }
             nx=x+2;
             ny=y-2;
@@ -146,11 +146,11 @@ bool highlight(int x, int y){
             else if(front_fields[x* 34 + y].name == "tower"){
                 for(int k = x-2; k <= x+2; k++){
                     for (int l = y-4; l <= y+4; l++){
-                        if(k>-1&&k<17&&l>=0&&l<34){
+                        if(k>=0&&k<17&&l>=0&&l<34){
                             if(canMove(front_fields, x, y, k, l))background_fields[k][l].setColor(sf::Color::Green);
                         }
                     }
-                int nx=x+2;
+                /*int nx=x+2;
                 int ny=y+2;
                 while(nx<17&&ny<34&&nx<x+4){
                     nx++;
@@ -183,7 +183,7 @@ bool highlight(int x, int y){
                 }
                 for(int i=y-8; i<=y-6; i+=2){
                     if(canAttack(front_fields, x, y, x, i))background_fields[x][i].setColor(sf::Color::Red);
-                }
+                }*/
                 }
             }
 
@@ -207,7 +207,7 @@ bool highlight(int x, int y){
             }
             if(x-2>0){
                 if(canMove(front_fields, x, y, x-2, y)  &&
-                    front_fields[(x-2)*34+y].name == empty)   
+                    front_fields[(x-2)*34+y].name == empty)
                         background_fields[x-2][y].setColor(sf::Color::Green);
             }
             if(x+2>0){
@@ -266,7 +266,7 @@ bool highlight(int x, int y){
                 front_fields[nx*34+ny].owner == opponentOwner)
                     background_fields[nx][ny].setColor(sf::Color::Red);
             else if(canMove(front_fields, x, y, nx, ny)     &&
-                front_fields[nx*34+ny].name == empty)   
+                front_fields[nx*34+ny].name == empty)
                     background_fields[nx][ny].setColor(sf::Color::Green);
             nx++;
             ny++;
@@ -312,7 +312,7 @@ bool highlight(int x, int y){
                 front_fields[x*34+i].owner == opponentOwner)
                     background_fields[x][i].setColor(sf::Color::Red);
             else if(canMove(front_fields, x, y,x, i)        &&
-                front_fields[x*34+i].name == empty)   
+                front_fields[x*34+i].name == empty)
                     background_fields[x][i].setColor(sf::Color::Green);
         }
 
